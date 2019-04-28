7bc09df38c689bb3f405be2ba40220d882c7701d
bandianhong <scottban@126.com>
2019-04-28 20:38:25 +0800

    Release 1.2.3

--- UIKit/View/BDTextField/BDTextField.m
+++ UIKit/View/BDTextField/BDTextField.m
@@ -21,6 +21,7 @@
  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  SOFTWARE.
  */
+#import <Foundation/Foundation.h>
 #import "BDTextField.h"
 #define DefaultSize 13
 
