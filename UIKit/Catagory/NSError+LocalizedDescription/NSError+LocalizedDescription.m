1c3192dbb3d7573db6e48703557c9897eee3112e
bandianhong <scottban@126.com>
2020-03-16 15:06:50 +0800

    Release 2.9.9

--- UIKit/Catagory/NSError+LocalizedDescription/NSError+LocalizedDescription.m
+++ UIKit/Catagory/NSError+LocalizedDescription/NSError+LocalizedDescription.m
@@ -22,6 +22,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 #import "NSError+LocalizedDescription.h"
+#import <objc/runtime.h>
 
 @implementation NSError (LocalizedDescription)
 
